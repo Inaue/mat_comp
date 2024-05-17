afb5ebb4baafc0767059e71b24a0ea9be90298fe
Inaue <100319268+Inaue@users.noreply.github.com>
2024-05-17 16:34:14 +0000

    Removing examples.

--- pds_i/testes-primos/main.c
+++ pds_i/testes-primos/main.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
-#include <math.h>
-#include <limits.h>
 #include "primalidade.h"
 
 #define SUCESSO 0
 
-int main(int argc, char** argv)
+int main();
+
+int main()
 {
-    unsigned long long n, q_testes = 100;
-
-    for(n = 0; n < 104730; n++)
-        if(solovay_strassen(n, q_testes))
-            printf("%llu ", n);
-
     return SUCESSO;
 }
